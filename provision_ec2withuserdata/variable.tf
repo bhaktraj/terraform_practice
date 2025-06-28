ab4682a7be1dedea959486a9cc150e84c5841d2a
bhaktraj <ankitbhaktraj@gmail.com>
2025-06-28 20:18:58 +0530

    userdata add in automation

---
+++ provision_ec2withuserdata/variable.tf
@@ -0,0 +1,13 @@
+variable "ec2_instance_type" {
+    default = "t2.micro"
+    type = string
+}
+variable "ec2_instance_size" {
+    default = 8
+    type = number
+}
+variable "ec2_ami" {
+    default = "ami-020cba7c55df1f615"
+    type = string
+  
+}
